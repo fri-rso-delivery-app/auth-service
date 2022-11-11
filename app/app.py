6210887d84e5afa8ddbf7075cc51d168e8a59c27
Jan Leskovec <janleskovec1@gmail.com>
2022-11-11 11:30:32 +0100

    remove unused

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import HTMLResponse
 
 
